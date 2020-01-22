388fe6e91083f4d0cfdb6d551d57debef2093382
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-01-22 09:54:02 +0100

    Initial project add

---
+++ src/test/java/com/mibauzas/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibauzas.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
