5397c5f493bb13e73d0de4bf414c95722de81113
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-03 15:27:01 +0100

    Added specialities map impl. Closes #30

---
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,12 @@
+package com.mibauzas.sfgpetclinic.services;
+
+import com.mibauzas.sfgpetclinic.model.Speciality;
+
+/**
+ * SpecialtyService
+ */
+public interface SpecialityService extends CrudService<Speciality, Long>{
+
+    
+    
+}
