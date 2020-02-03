cbf3c3c94f5e9632e3c18f39dd253627cc2b1283
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-03 12:46:44 +0100

    Added PetType service. Closes #28

---
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package com.mibauzas.sfgpetclinic.services;
+
+import com.mibauzas.sfgpetclinic.model.PetType;
+
+/**
+ * PetTypeService
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+    
+}
