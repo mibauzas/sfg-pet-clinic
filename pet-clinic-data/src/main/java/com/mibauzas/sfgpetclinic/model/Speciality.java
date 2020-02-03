0c2995dadc9f91cdd4e9bbe945d4cfc5fd7d0870
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-03 12:27:47 +0100

    Added Speciality entity, added to Vet. Closes #26

---
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,19 @@
+package com.mibauzas.sfgpetclinic.model;
+
+/**
+ * Speciality
+ */
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+
+}
--- pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Vet.java
@@ -1,9 +1,20 @@
 package com.mibauzas.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * created by mbs on 01/24/20
  */
 public class Vet extends Person{
 
-    
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
 }
