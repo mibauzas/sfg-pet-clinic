94543b7f1ab42fbfb06657f7d64ccdcbb91fe062
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-03 12:19:25 +0100

    Added Visit, added pet list to owner. Closes #26

--- pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Owner.java
@@ -1,9 +1,22 @@
 package com.mibauzas.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * created by mbs on 01/24/20
  */
 public class Owner extends Person{
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    
     
 }
