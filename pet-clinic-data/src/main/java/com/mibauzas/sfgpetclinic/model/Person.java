8acf1729d4d195c0846f11fb1956c0481c73d78d
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-05 22:30:38 +0100

    Converts owners to JPA entity. Closes #35

--- pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/Person.java
@@ -1,11 +1,18 @@
 package com.mibauzas.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * created by mbs on 01/24/20
  */
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/model/PetType.java
@@ -1,10 +1,17 @@
 package com.mibauzas.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * created by mbs on 01/24/20
  */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
