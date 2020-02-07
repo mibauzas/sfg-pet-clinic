ddb18821832a8bf22db6e86c47da2aa56196b121
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-07 11:17:01 +0100

    Implements visit map service. Closes #45

---
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package com.mibauzas.sfgpetclinic.services;
+
+import com.mibauzas.sfgpetclinic.model.Visit;
+
+/**
+ * VisitService
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+
+    
+}
