59aad6b9406dbb8b37070cf2cb05f4793cae857f
Miquel Bauza Soriano <mibauzas@gmail.com>
2020-02-07 10:46:43 +0100

    Adds spring data jpa speciality service Closes #43

---
+++ pet-clinic-data/src/main/java/com/mibauzas/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,55 @@
+package com.mibauzas.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mibauzas.sfgpetclinic.model.Speciality;
+import com.mibauzas.sfgpetclinic.repositories.SpecialityRepository;
+import com.mibauzas.sfgpetclinic.services.SpecialityService;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * SpecialitySDJpaService
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    
+}
